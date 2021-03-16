e319afae395101b4fc823799358ce92b51aee1a8
Daniel Alm Grundström <daniel.alm.grundstrom@protonmail.com>
2021-03-16 10:40:43 +0100

    Project setup

---
+++ src/main/java/se/lexicon/almgru/assignmentjpaj33/AssignmentJpaJ33Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.almgru.assignmentjpaj33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentJpaJ33Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentJpaJ33Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/almgru/assignmentjpaj33/AssignmentJpaJ33ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.almgru.assignmentjpaj33;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentJpaJ33ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
